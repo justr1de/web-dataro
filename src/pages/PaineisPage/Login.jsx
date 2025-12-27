ff4ac34591823c068e3751023a4862256745f04a
justr1de <ranieri.braga@hotmail.com>
2025-12-27 02:47:52 -0400

    style: Update login header to blue version

--- src/pages/PaineisPage/Login.jsx
+++ src/pages/PaineisPage/Login.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { getBandeiraUrl } from '../../utils/bandeirasMap';
 import logo from '../../assets/logo.png';
 import bandeiraRondonia from '../../assets/bandeira-rondonia.png';
-import headerImage from '../../assets/header-login.svg';
+import headerImage from '../../assets/header-login-blue.png';
 import ThemeToggle from '../../components/ThemeToggle';
 import './Login.css';
 
