f5683a0074b8c64d61d243e603dd71fc975ded89
justr1de <ranieri.braga@hotmail.com>
2025-12-27 09:27:50 -0400

    feat: change login header to header-op3

--- src/pages/PaineisPage/Login.jsx
+++ src/pages/PaineisPage/Login.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { getBandeiraUrl } from '../../utils/bandeirasMap';
 import logo from '../../assets/logo.png';
 import bandeiraRondonia from '../../assets/bandeira-rondonia.png';
-import headerImage from '../../assets/header-opt2.png';
+import headerImage from '../../assets/header-op3.png';
 import ThemeToggle from '../../components/ThemeToggle';
 import './Login.css';
 
