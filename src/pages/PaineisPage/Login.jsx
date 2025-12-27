2e3135ad898cede219635bf2c37980e19eb6f9cf
justr1de <ranieri.braga@hotmail.com>
2025-12-27 03:54:36 -0400

    style: Update header to hhheader.png with cover to preserve graphics

--- src/pages/PaineisPage/Login.jsx
+++ src/pages/PaineisPage/Login.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { getBandeiraUrl } from '../../utils/bandeirasMap';
 import logo from '../../assets/logo.png';
 import bandeiraRondonia from '../../assets/bandeira-rondonia.png';
-import headerImage from '../../assets/header-last-tonight.png';
+import headerImage from '../../assets/hhheader.png';
 import ThemeToggle from '../../components/ThemeToggle';
 import './Login.css';
 
